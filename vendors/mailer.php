1f81df6ac3db4a808b76d2a5b9065471a92a397f
Cauan Cabral <cauan@radig.com.br>
2011-07-01 16:06:25 -0400

    Remoção do método batch de envio
    Remoção do método batch de envio, para manter plugin compatível com versão 4.1 do SwiftMailer
    Remoção da atribuição por referência em objeto instanciado, garantindo compatibilidade com PHP 5.3.x

--- vendors/mailer.php
+++ vendors/mailer.php
@@ -9,16 +9,16 @@ App::import('Core', 'View');
 class Mailer extends Object
 {
 	protected $sender = null;
-	
+
 	protected $message = null;
-	
+
 	protected $Controller = null;
-	
+
 	public $failures = array();
 
 	/**
 	 * Array com opções de configuração para o Component, possuindo os seguintes índices:
-	 * 
+	 *
 	 * - transport: php {php|sendmail|smtp}
 	 * - smtp: array {configuração do SMTP, caso seja o transport utilizado}
 	 *   - port: 25 {defina a porta usada para conexão SMTP}
@@ -27,7 +27,6 @@ class Mailer extends Object
 	 * - sendmail: array {configuração do Sendmail, caso seja o transport utilizado}
 	 *   - path: /usr/sbin/sendmail {local com o binário do sendmail}
 	 *   - params: '' {parâmetros que serão passados ao sendmail}
-	 * - batch: true {true|false}
 	 * - contentType: html {text|html}
 	 * - template: default {nome da pasta com template desejado}
 	 * - layout: default {nome do layout desejado}
@@ -46,43 +45,42 @@ class Mailer extends Object
 			'path' => '/usr/sbin/sendmail',
 			'params' => ''
 		),
-		'batch' => true,
 		'contentType' => 'html',
 		'template' => 'default',
 		'layout' => 'default',
 		'confirmReceipt' => false
 	);
-	
+
 	// construtor default
 	public function __construct($settings = array(), $controller = null)
 	{
 		$this->setOptions($settings);
-		
+
 		if($controller !== null)
 		{
 			$this->Controller = $controller;
 		}
 	}
-	
+
 	// seta as configurações
 	public function setOptions($options)
 	{
 		$this->options = Set::merge($this->options, $options);
-		
+
 		if($this->options['layout'])
 			$this->layout = $this->options['layout'];
-			
+
 		if($this->options['template'])
 			$this->template = $this->options['template'];
-			
-		
+
+
 	}
-	
-	/*************** Begin utils funcions ***************/ 
-	
+
+	/*************** Begin utils funcions ***************/
+
 	/**
 	 * Send one or more message
-	 * 
+	 *
 	 * @param array $options índices válidos são:
 	 * 	 - 'to': string ou array com endereços de email do destinatário - REQUIRED
 	 * 	 - 'from': string com email do remetente - REQUIRED
@@ -92,7 +90,7 @@ class Mailer extends Object
 	 *   - 'attachments': array
 	 *     - 'path': string
 	 *     - 'type': string
-	 *  
+	 *
 	 * @return bool
 	 */
 	public function sendMessage($options = array())
@@ -100,37 +98,34 @@ class Mailer extends Object
 		if(empty($options))
 		{
 			trigger_error(__('$options não pode estar vazio', TRUE), E_USER_ERROR);
-			
+
 			return FALSE;
 		}
-		
+
 		if(!$this->__configureTransport())
 		{
 			trigger_error(__('Falha na configuração do transporte', TRUE), E_USER_WARNING);
 			return FALSE;
 		}
-		
+
 		if(!$this->__setMessageOptions($options))
 		{
 			trigger_error(__('Falha na definição da mensagem', TRUE), E_USER_WARNING);
 			return FALSE;
 		}
-		
-		if($this->options['batch'])
-			return $this->sender->batchSend($this->message, $this->failures);
-		else
+
 			return $this->sender->send($this->message, $this->failures);
 	}
 	/**************** End utils funcions ****************/
-	
-	
+
+
 	/****************** Begin setters *******************/
-	
+
 	/**
 	 * Habilita o plugin AntiFlood que permite interromper
 	 * o envio de mensagens inserindo uma pausa quando
 	 * for ultrapassado determinado limite.
-	 * 
+	 *
 	 * @param int $limit limite de envio em sequência
 	 * @param int $pause tempo de pausa em segundos
 	 */
@@ -138,14 +133,14 @@ class Mailer extends Object
 	{
 		$this->sender->registerPlugin(new Swift_Plugins_AntiFloodPlugin($limit, $pause));
 	}
-	
+
 	/**
 	 * Habilita o plugin Throttler que permite controlar
 	 * a velocidade de envio das mensagens
-	 * 
+	 *
 	 * @param int $limit Limite de mensagens ou bytes
 	 * @param string $type Tipo de limite, valores válidos são: 'message' e 'bytes'
-	 * 
+	 *
 	 */
 	public function enableThrottler($limit = 100, $type = 'message')
 	{
@@ -169,12 +164,12 @@ class Mailer extends Object
 		if($this->message === null)
 		{
 			trigger_error(__('Não é possível setar uma propriedade antes de criar uma mensagem', TRUE), E_USER_ERROR);
-			
+
 			return FALSE;
 		}
-		
+
 		$this->message->setSubject($value);
-		
+
 		return TRUE;
 	}
 
@@ -188,13 +183,13 @@ class Mailer extends Object
 		if($this->message === null)
 		{
 			trigger_error(__('Não é possível setar uma propriedade antes de criar uma mensagem', TRUE), E_USER_ERROR);
-			
+
 			return FALSE;
 		}
-		
+
 		$decorator = new Swift_Plugins_DecoratorPlugin($replacements);
 		$this->sender->registerPlugin($decorator);
-		
+
 		if($this->options['contentType'] == 'html')
 		{
 			$this->message->setBody($this->__render($value), 'text/html');
@@ -203,7 +198,7 @@ class Mailer extends Object
 		{
 			$this->message->setBody($this->__render($value), 'text/plain');
 		}
-		
+
 		return TRUE;
 	}
 
@@ -217,12 +212,12 @@ class Mailer extends Object
 		if($this->message === null)
 		{
 			trigger_error(__('Não é possível setar uma propriedade antes de criar uma mensagem', TRUE), E_USER_ERROR);
-			
+
 			return FALSE;
 		}
-		
+
 		$this->message->setPart($value);
-		
+
 		return TRUE;
 	}
 	/******************* End setters *********************/
@@ -239,32 +234,32 @@ class Mailer extends Object
 		if($this->message === null)
 		{
 			$this->message = Swift_Message::newInstance();
-			
+
 			return ($this->message != null);
 		}
-		
+
 		return TRUE;
 	}
-	
+
 	/**
 	 * Define todas as opções fornecidas como propriedades da mensagem
 	 * Se uma propriedade é Obrigatório, mas não está definida em $options, o método
 	 * aborta e retorna FALSE, caso contrário, retorna TRUE
-	 * 
+	 *
 	 * @param array $options - REQUIRED
 	 * @return bool
 	 */
 	private function __setMessageOptions($options = array())
 	{
 		$status = TRUE;
-		
+
 		if(!$this->__initMessage())
-		{	
+		{
 			trigger_error(__('Não é possível setar uma propriedade antes de criar uma mensagem', TRUE), E_USER_ERROR);
-			
+
 			return FALSE;
 		}
-		
+
 		// define destinatário do email
 		if(isset($options['to']))
 		{
@@ -275,12 +270,18 @@ class Mailer extends Object
 			if(!isset($options['bcc']) && !isset($options['cc']))
 			{
 				trigger_error(__('É preciso definir o destinatário da mensagem', TRUE), E_USER_ERROR);
-			
+
 				return FALSE;
 			}
 		}
 		
-		// define origem do email
+		// define origem efetiva do email
+		if(isset($options['sender']))
+		{
+			$this->message->setSender($options['sender']);
+		}
+
+		// define quem aparecerá como remetente da mensagem
 		if(isset($options['from']))
 		{
 			$this->message->setFrom($options['from']);
@@ -291,31 +292,31 @@ class Mailer extends Object
 				$this->message->setReadReceiptTo($options['from']);
 			}
 		}
-		else
+		else if(!isset($options['sender']))
 		{
 			trigger_error(__('É preciso definir o remetente da mensagem', TRUE), E_USER_ERROR);
-			
+
 			return FALSE;
 		}
-		
+
 		// define email's que receberam cópia-carbono
 		if(isset($options['cc']))
 		{
 			$status = ($status && $this->message->setCc($options['cc']));
 		}
-		
+
 		// define email's que receberam cópia-carbono oculta
 		if(isset($options['bcc']))
 		{
 			$status = ($status && $this->message->setBcc($options['bcc']));
 		}
-		
+
 		// define o assunto
 		if(isset($options['subject']))
 		{
 			$status = ($status && $this->setMessageSubject($options['subject']));
 		}
-		
+
 		// define conteúdo da mensagem
 		if(isset($options['body']))
 		{
@@ -328,7 +329,7 @@ class Mailer extends Object
 				$status = ($status && $this->setMessageBody($options['body']));
 			}
 		}
-		
+
 		// define tipo do conteúdo da mensagem
 		switch($this->options['contentType'])
 		{
@@ -345,7 +346,7 @@ class Mailer extends Object
 		{
 			$status = ($status && $this->__attachFiles($options['attachments']));
 		}
-		
+
 		return $status;
 	}
 
@@ -364,18 +365,18 @@ class Mailer extends Object
 			else
 			{
 				trigger_error(__('Algum anexo foi passado incorretamente.', true), E_USER_ERROR);
-				
+
 				return false;
 			}
 		}
 
 		return true;
 	}
-	
+
 	/**
 	 * Recupera parâmetros relacionados ao transporte em Mailer::options
 	 * e inicia a class Swift_Transport
-	 * 
+	 *
 	 * @return bool
 	 */
 	private function __configureTransport($override = false)
@@ -384,7 +385,7 @@ class Mailer extends Object
 		{
 			return $this->sender;
 		}
-		
+
 		if($this->options['transport'] == 'smtp')
 		{
 			if(!empty($this->options['smtp']['encryption']))
@@ -392,15 +393,15 @@ class Mailer extends Object
 				$transport =
 					Swift_SmtpTransport::newInstance($this->options['smtp']['host'], $this->options['smtp']['port'], $this->options['smtp']['encryption']);
 			}
-			else 
+			else
 			{
 				$transport =
 					Swift_SmtpTransport::newInstance($this->options['smtp']['host'], $this->options['smtp']['port']);
 			}
-				
+
 			if(isset($this->options['smtp']['username']))
 				$transport->setUsername($this->options['smtp']['username']);
-				
+
 			if(isset($this->options['smtp']['password']))
 				$transport->setPassword($this->options['smtp']['password']);
 		}
@@ -418,104 +419,104 @@ class Mailer extends Object
 			trigger_error(__('Camada de transporte inválida', TRUE), E_USER_ERROR);
 			return FALSE;
 		}
-		
+
 		// Define a sender based on transport
 		$this->sender = new Swift_Mailer($transport);
-		
+
 		return TRUE;
 	}
-	
+
 	/**
 	 * Render the contents using the current layout and template.
 	 * Based on EmailComponent, part of CakePHP Framework
-	 * 
+	 *
 	 * @copyright EmailComponent: CakePHP Foundation
 	 * @link http://cakephp.org
 	 * @subpackage cake.libs.controllers.components.email
 	 * @license MIT
-	 * 
+	 *
 	 * @param string $content Conteúdo que será renderizado
-	 * 
+	 *
 	 * @return array Email ready to be sent
 	 * @access private
 	 */
 	private function __render($content)
 	{
 		$body = '';
-		
+
 		if($this->Controller === null)
 		{
 			App::import('Core', 'Controller');
-			
+
 			$this->Controller = new Controller();
 		}
-		
+
 		$viewClass = $this->Controller->view;
-		
+
 		if ($this->Controller->view != 'View')
 		{
 			list($plugin, $viewClass) = pluginSplit($viewClass);
 			$viewClass = $viewClass . 'View';
 			App::import('View', $this->Controller->view);
 		}
-		
-		$View =& new $viewClass($this->Controller);
-		
+
+		$View = new $viewClass($this->Controller);
+
 		$View->layout = $this->layout;
-		
+
 		if (is_array($content))
 		{
 			$content = implode("\n", $content) . "\n";
 		}
-				
-		if ($this->options['contentType'] === 'html') 
+
+		if ($this->options['contentType'] === 'html')
 		{
 			$View->layoutPath = 'email' . DS . 'html';
-			
+
 			$body = $View->element('email' . DS . 'html' . DS . $this->template, array('content' => $content), true);
-			
+
 			$body = str_replace(array("\r\n", "\r"), "\n", $View->renderLayout($body));
-			
+
 			$body = $this->__embedImages($body);
 		}
 		else if ($this->options['contentType'] === 'text')
 		{
 			$View->layoutPath = 'email' . DS . 'text';
-			
+
 			$body = $View->element('email' . DS . 'text' . DS . $this->template, array('content' => $content), true);
 
 			$body = str_replace(array("\r\n", "\r"), "\n", $View->renderLayout($body));
 		}
-		
+
 		return $body;
 	}
-	
+
 	/**
 	 * Recebe uma string com o conteúdo da mensagem e retorna
 	 * a string com as imagens encontradas por equivalentes na forma
 	 * de 'anexo' inline
-	 * 
+	 *
 	 * @param string $content
-	 * 
+	 *
 	 * @return string $content
 	 */
 	private function __embedImages($content)
 	{
 		$dom = new DOMDocument();
-		
+
 		$dom->loadHtml($content);
-		
+
 		$imgs = $dom->getElementsByTagName('img');
-		
+
 		foreach($imgs as $img)
 		{
 			$src = Swift_Image::fromPath($img->getAttribute('src'));
-			
+
 			$img->setAttribute('src', $this->message->embed($src));
 		}
-		
+
 		$content = $dom->saveHTML();
-		
+
 		return $content;
 	}
 }
